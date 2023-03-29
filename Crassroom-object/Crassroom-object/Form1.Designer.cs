573e6896868c3aa8baafaae675cd758cfbc227a7
pond1111 <127219087+pond1111@users.noreply.github.com>
2023-03-30 02:45:29 +0700

    Crassroom-Object

---
+++ Crassroom-object/Crassroom-object/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Crassroom_object
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxgpa = new System.Windows.Forms.TextBox();
+            this.textBoxBY = new System.Windows.Forms.TextBox();
+            this.textBoxMax = new System.Windows.Forms.TextBox();
+            this.textBoxMin = new System.Windows.Forms.TextBox();
+            this.textBoxAvg = new System.Windows.Forms.TextBox();
+            this.textBoxAgeAll = new System.Windows.Forms.TextBox();
+            this.textBoxListName = new System.Windows.Forms.TextBox();
+            this.textBoxMaxName = new System.Windows.Forms.TextBox();
+            this.textBoxMinName = new System.Windows.Forms.TextBox();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ชื่อ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ปีเกิด";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "GPA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(262, 50);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "GPA Max";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(265, 90);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "GPA Min";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(265, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "GPA Avg";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(241, 193);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(89, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "อายุรวมในห้อง";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(11, 240);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "รายชื่อ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(546, 90);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "ชื่อ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(546, 47);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 20);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "ชื่อ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(64, 47);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxName.TabIndex = 10;
+            // 
+            // textBoxgpa
+            // 
+            this.textBoxgpa.Location = new System.Drawing.Point(64, 87);
+            this.textBoxgpa.Name = "textBoxgpa";
+            this.textBoxgpa.Size = new System.Drawing.Size(125, 27);
+            this.textBoxgpa.TabIndex = 11;
+            // 
+            // textBoxBY
+            // 
+            this.textBoxBY.Location = new System.Drawing.Point(64, 128);
+            this.textBoxBY.Name = "textBoxBY";
+            this.textBoxBY.Size = new System.Drawing.Size(125, 27);
+            this.textBoxBY.TabIndex = 12;
+            // 
+            // textBoxMax
+            // 
+            this.textBoxMax.Location = new System.Drawing.Point(336, 47);
+            this.textBoxMax.Name = "textBoxMax";
+            this.textBoxMax.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMax.TabIndex = 13;
+            // 
+            // textBoxMin
+            // 
+            this.textBoxMin.Location = new System.Drawing.Point(336, 90);
+            this.textBoxMin.Name = "textBoxMin";
+            this.textBoxMin.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMin.TabIndex = 14;
+            // 
+            // textBoxAvg
+            // 
+            this.textBoxAvg.Location = new System.Drawing.Point(337, 132);
+            this.textBoxAvg.Name = "textBoxAvg";
+            this.textBoxAvg.Size = new System.Drawing.Size(125, 27);
+            this.textBoxAvg.TabIndex = 15;
+            // 
+            // textBoxAgeAll
+            // 
+            this.textBoxAgeAll.Location = new System.Drawing.Point(336, 190);
+            this.textBoxAgeAll.Name = "textBoxAgeAll";
+            this.textBoxAgeAll.Size = new System.Drawing.Size(125, 27);
+            this.textBoxAgeAll.TabIndex = 16;
+            // 
+            // textBoxListName
+            // 
+            this.textBoxListName.Location = new System.Drawing.Point(64, 240);
+            this.textBoxListName.Multiline = true;
+            this.textBoxListName.Name = "textBoxListName";
+            this.textBoxListName.Size = new System.Drawing.Size(302, 174);
+            this.textBoxListName.TabIndex = 17;
+            // 
+            // textBoxMaxName
+            // 
+            this.textBoxMaxName.Location = new System.Drawing.Point(578, 44);
+            this.textBoxMaxName.Name = "textBoxMaxName";
+            this.textBoxMaxName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMaxName.TabIndex = 18;
+            // 
+            // textBoxMinName
+            // 
+            this.textBoxMinName.Location = new System.Drawing.Point(578, 90);
+            this.textBoxMinName.Name = "textBoxMinName";
+            this.textBoxMinName.Size = new System.Drawing.Size(125, 27);
+            this.textBoxMinName.TabIndex = 19;
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.buttonEnter.Location = new System.Drawing.Point(64, 161);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(125, 52);
+            this.buttonEnter.TabIndex = 20;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = false;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.textBoxMinName);
+            this.Controls.Add(this.textBoxMaxName);
+            this.Controls.Add(this.textBoxListName);
+            this.Controls.Add(this.textBoxAgeAll);
+            this.Controls.Add(this.textBoxAvg);
+            this.Controls.Add(this.textBoxMin);
+            this.Controls.Add(this.textBoxMax);
+            this.Controls.Add(this.textBoxBY);
+            this.Controls.Add(this.textBoxgpa);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBoxName;
+        private TextBox textBoxgpa;
+        private TextBox textBoxBY;
+        private TextBox textBoxMax;
+        private TextBox textBoxMin;
+        private TextBox textBoxAvg;
+        private TextBox textBoxAgeAll;
+        private TextBox textBoxListName;
+        private TextBox textBoxMaxName;
+        private TextBox textBoxMinName;
+        private Button buttonEnter;
+    }
+}
---
+++ Crassroom-object/Crassroom-object/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crassroom_object
+{
+    internal class Person
+    {
+        public string name { get; set; }
+        public double gpa { get; set; }
+        public int birth { get; set; }
+
+        public Person(string name, double gpa, int birth)
+        {
+            this.name = name;
+            this.gpa = gpa;
+            this.birth = 2566 - birth;
+        }
+    }
+}
